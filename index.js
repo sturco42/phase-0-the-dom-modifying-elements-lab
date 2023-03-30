8ffb63abd746581da1001591a18992f7657aa64b
Sam <samturco42@gmail.com>
2023-03-30 10:58:12 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector("body #main");
+console.log(main);
+main.remove();
+
+
+const newHeader = document.createElement("h1")
+console.log("step 1: ", newHeader)
+
+newHeader.id = "victory"
+console.log("step 2: ", newHeader)
+newHeader.textContent = "Sam is the champion"
+console.log("step 3: ", newHeader)
